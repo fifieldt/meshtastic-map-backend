44d43cbf3f5fce1e86a0bb186dd9378d1515f4ff
Tom Fifield <tom@tomfifield.net>
2024-08-23 18:03:23 +0800

    Move coordinate check log from info to debug

--- backend.py
+++ backend.py
@@ -110,7 +110,7 @@ def processPosition(pktfrom, data, max_precision=16):
         londelta = radians(geoItoFloat(data["longitudeI"])) - radians(cliargs.longitude)
         a = sin(latdelta / 2)**2 + cos(radians(cliargs.latitude)) * cos(radians(geoItoFloat(data["latitudeI"]))) * sin(londelta / 2)**2
         if 6373.0 * (2 * atan2(sqrt(a), sqrt(1 - a))) > cliargs.max_distance:
-            logging.info("Coordinate Check Failed latcheck=%f, longcheck=%f, %s" %(latdelta, londelta, data))
+            logging.debug("Coordinate Check Failed latcheck=%f, longcheck=%f, %s" %(latdelta, londelta, data))
             return
 
     if pktfrom not in nodes.keys():
